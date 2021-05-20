8f280e439fff39bd8b220aef0a5f75853daea332
elliottgreen <82888909+elliottgreen@users.noreply.github.com>
2021-05-20 10:28:00 -0700

    @ADD:  Solution for 2.6.1.9
    This is one way to solve the problem statement in lab 2.6.1.9.

---
+++ Module2/SimpleInputOutput.py
@@ -0,0 +1,20 @@
+# input a float value for variable a here
+float_variable_a = float(input("input a float value for variable a here: "))
+
+# input a float value for variable b here
+float_variable_b = float(input("input a float value for variable b here: "))
+
+# output the result of addition here
+print("Sum of a, b = ", float_variable_a + float_variable_b)
+
+# output the result of subtraction here
+print("Difference of a, b = ", float_variable_a - float_variable_b)
+
+# output the result of multiplication here
+print("Product of a, b = ", float_variable_a * float_variable_b)
+
+# output the result of division here
+print("Quotient of a, b = ", float_variable_a / float_variable_b)
+
+# Parting saluation. 
+print("\nThat's all, folks!")
