8161576c433d51de311205e38cf355b598b7a1ac
elliottgreen <omar.elliottgreen1@ieee.org>
2021-05-26 11:04:51 -0700

    @ADD: Flowers scaffold

---
+++ Module3/Flowers.py
@@ -0,0 +1,9 @@
+#Please submit some suggested code
+'''
+Imagine that your computer program loves these plants. 
+Whenever it receives an input in the form of the word Spathiphyllum, 
+it involuntarily shouts to the console the following string: 
+    "Spathiphyllum is the best plant ever!"
+
+From Lab 3.1.1.10 
+'''
